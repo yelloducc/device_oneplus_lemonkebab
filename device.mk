548ec0a288a03be7f78c96cdd53488e3058f3bef
Marc Bourgoin <themard85@gmail.com>
2021-11-06 09:59:06 +0530

    lemonkebab: Switch to AOSP WFD

--- device.mk
+++ device.mk
@@ -530,11 +530,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
-    libnl \
-    libwfdaac_vendor
-
-PRODUCT_BOOT_JARS += \
-    WfdCommon
+    libnl
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-wfd.xml
